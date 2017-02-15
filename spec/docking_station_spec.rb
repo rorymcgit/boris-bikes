2d1a955a651140ee2116af0ce6c63193a8a5f7cd
Ryan <Ryanchu.ws@gmail.com>
2017-02-15 17:20:40 +0000

    Up to challenge 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,8 +5,11 @@ describe DockingStation do
       expect(subject).to respond_to :release_bike
     end
 
-    it "releases working bikes" do
-      bike = subject.release_bike
-      expect(bike).to be_working
-    end
+
+  it "releases working bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  it { is_expected.to respond_to(:dock_bike).with(1).argument}
   end
