6b7fcf82f94a1920996c04523cdd31795a78287a
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-02-17 09:28:39 +0000

    amended comments slightly

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -16,10 +16,8 @@ class DockingStation
   end
 
   def dock_bike(bike1)
-  #nb - Bike is created outside func.; Dock capcity = 1.
-  #this method will send error if @bike already exists
-  #@bike will only exist if a bike has already been docked
-  #hence that bike will have become instance variable
+  # This method will send error if 20 bikes have been docked
+  # and it therefore cannot accept any more.
     fail "I iz full" if @bikes_arr.length >= 20
     @bikes_arr << bike1
     @bike = bike1
