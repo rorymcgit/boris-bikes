d350b9bb0843d6e9c0756541cb362494e6390b06
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-02-17 10:34:51 +0000

    refactored to include private full and empty methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -11,16 +11,25 @@ class DockingStation
   end
 
   def release_bike
-    fail "no bikes" if @bikes_arr.empty?
+    fail "no bikes" if empty?
     @bikes_arr.pop
   end
 
   def dock_bike(bike1)
   # This method will send error if 20 bikes have been docked
   # and it therefore cannot accept any more.
-    fail "I iz full" if @bikes_arr.length >= 20
+    fail "I iz full" if full?
     @bikes_arr << bike1
     @bike = bike1
   end
+
+  private
+    def full?
+      @bikes_arr.length >= 20 ? true : false
+    end
+
+    def empty?
+      @bikes_arr.empty? ? true : false
+    end
 
 end
